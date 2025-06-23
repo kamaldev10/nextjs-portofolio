091d8a9aaca34741e1a1908d823ef76288bcd980
Ali Musthafa Kamal <alimusthafakamal@gmail.com>
2025-06-23 19:39:47 +0700

    latest update

--- src/app/projects/[slug]/page.tsx
+++ src/app/projects/[slug]/page.tsx
@@ -4,28 +4,23 @@ import { projects } from "@/lib/data/projectsData";
 import { Metadata } from "next";
 import { notFound } from "next/navigation";
 
-// generateMetadata tetap di halaman ini karena digunakan untuk SEO
-export async function generateMetadata({
-  params,
-}: {
-  params: { slug: string };
-}): Promise<Metadata> {
-  const project = projects.find((p) => p.slug === params.slug);
-  if (!project) {
-    return { title: "Project Not Found" };
-  }
-  return {
-    title: project.title,
-    description: project.description,
-  };
-}
+// // SEO Metadata
+// export function generateMetadata({
+//   params,
+// }: {
+//   params: { slug: string };
+// }): Metadata {
+//   const project = projects.find((p) => p.slug === params.slug);
+//   if (!project) {
+//     return { title: "Project Not Found" };
+//   }
+//   return {
+//     title: project.title,
+//     description: project.description,
+//   };
+// }
 
-// Komponen utama dipecah agar bersih dan modular
-export default async function ProjectPage({
-  params,
-}: {
-  params: { slug: string };
-}) {
+export default function ProjectPage({ params }: { params: { slug: string } }) {
   const project = projects.find((p) => p.slug === params.slug);
   if (!project) return notFound();
 
