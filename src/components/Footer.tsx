74831cbc86293503b0fc77cf1e89d5c3363c9740
Ali Musthafa Kamal <alimusthafakamal@gmail.com>
2025-10-01 17:20:59 +0700

    refactor: remove social media link

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,40 +1,12 @@
 // components/Footer.tsx
-import Link from "next/link";
-import { FaGithub, FaLinkedinIn, FaInstagram } from "react-icons/fa";
 
 const Footer = () => {
   const currentYear = new Date().getFullYear();
 
   return (
-    <footer className="bg-slate-200 dark:bg-gray-900 py-4">
+    <footer className="bg-slate-200 dark:bg-slate-800 py-4">
       <div className="text-sm max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 text-center text-gray-900 dark:text-gray-300">
         <p>&copy; {currentYear} Ali Musthafa Kamal. All rights reserved.</p>
-        <div className="mt-2 flex justify-center space-x-2">
-          <Link
-            href="https://github.com/kamaldev10"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="hover:text-accent dark:hover:text-indigo-400 transition-colors"
-          >
-            <FaGithub />
-          </Link>
-          <Link
-            href="https://linkedin.com/in/alimusthafakamal"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="hover:text-accent dark:hover:text-indigo-400 transition-colors"
-          >
-            <FaLinkedinIn />
-          </Link>
-          <Link
-            href="https://instagram.com/alimusthafa10"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="hover:text-accent dark:hover:text-indigo-400 transition-colors"
-          >
-            <FaInstagram />
-          </Link>
-        </div>
       </div>
     </footer>
   );
