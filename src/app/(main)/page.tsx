ce5ad581e3c1a99d2636903f6cab42d3eb17e2ea
Ali Musthafa Kamal <alimusthafakamal@gmail.com>
2025-10-01 17:21:38 +0700

    refactor: move skill section to hero section bottom

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,21 +1,19 @@
 "use client";
 
-import HeroSection from "@/components/sections/HeroSection";
 import AboutSection from "@/components/sections/AboutSection";
 import ProjectsSection from "@/components/sections/ProjectsSection";
-// import ContactSection from "@/components/sections/ContactSection";
 import SkillsSection from "@/components/sections/SkillsSection";
 import CertificatesSection from "@/components/sections/CertificateSection";
+import HeroSection from "@/components/sections/HeroSection";
 
 export default function HomePage() {
   return (
     <div className=" w-full mx-auto">
       <HeroSection />
-      <AboutSection />
       <SkillsSection />
+      <AboutSection />
       <CertificatesSection />
       <ProjectsSection />
-      {/* <ContactSection /> */}
     </div>
   );
 }
