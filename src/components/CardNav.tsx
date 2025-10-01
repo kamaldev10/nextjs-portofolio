59e3f982cb9951bb8105d0c69de3ff8af5299a3f
Ali Musthafa Kamal <alimusthafakamal@gmail.com>
2025-10-02 00:23:17 +0700

    fix: use void in contentEl.offsetHeight

--- src/components/CardNav.tsx
+++ src/components/CardNav.tsx
@@ -65,7 +65,7 @@ const CardNav: React.FC<CardNavProps> = ({
         contentEl.style.position = "static";
         contentEl.style.height = "auto";
 
-        contentEl.offsetHeight;
+        void contentEl.offsetHeight;
 
         const topBar = 60;
         const padding = 16;
