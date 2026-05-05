32e3141d385c56de93ce2ad0066f39c554e1773e
Ali Musthafa Kamal <alimusthafakamal@gmail.com>
2026-05-06 00:26:48 +0700

    style: add 2025 - current date (year)

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,7 +6,10 @@ const Footer = () => {
   return (
     <footer className="bg-slate-200 dark:bg-slate-800 py-4">
       <div className="text-sm max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 text-center text-gray-900 dark:text-gray-300">
-        <p>&copy; {currentYear} Ali Musthafa Kamal. All rights reserved.</p>
+        <p>
+          &copy; <strong> 2025 - {currentYear} </strong> Ali Musthafa Kamal. All
+          rights reserved.
+        </p>
       </div>
     </footer>
   );
