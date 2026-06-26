a8e1427f13f74edd6720673e5069ebd984d55c60
Ali Musthafa Kamal <alimusthafakamal@gmail.com>
2026-06-27 00:17:00 +0700

    feat: change Gallery component, add contact section, reorder about section

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,21 +1,23 @@
-"use client";
+'use client';
 
-import AboutSection from "@/components/sections/AboutSection";
-import ProjectsSection from "@/components/sections/ProjectsSection";
-import SkillsSection from "@/components/sections/SkillsSection";
-import CertificatesSection from "@/components/sections/CertificateSection";
-import HeroSection from "@/components/sections/HeroSection";
-import RollingGallery from "@/components/RollingGallery";
+import AboutSection from '@/components/sections/AboutSection';
+import ProjectsSection from '@/components/sections/ProjectsSection';
+import SkillsSection from '@/components/sections/SkillsSection';
+import CertificatesSection from '@/components/sections/CertificateSection';
+import HeroSection from '@/components/sections/HeroSection';
+import ContactSection from '@/components/sections/ContactSection';
+import GallerySection from '@/components/sections/GallerySection';
 
 export default function HomePage() {
   return (
-    <div className=" w-full mx-auto">
+    <div className=' w-full mx-auto'>
       <HeroSection />
-      <SkillsSection />
       <AboutSection />
+      <SkillsSection />
       <ProjectsSection />
       <CertificatesSection />
-      <RollingGallery autoplay={true} pauseOnHover={true} />
+      <ContactSection />
+      <GallerySection />
     </div>
   );
 }
